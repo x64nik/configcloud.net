70010561fb8368675a788c8b4b0d400aeacc98de
x64nik <rushidarunte123@gmail.com>
2025-02-17 00:15:47 +0530

    added analytics

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,5 @@
 import { Loader2 } from 'lucide-react'
-
+import { Analytics } from "@vercel/analytics/react"
 export default function Loading() {
   return (
     <div className="flex items-center justify-center h-full">
